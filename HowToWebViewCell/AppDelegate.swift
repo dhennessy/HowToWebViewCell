d1e25637a45a0755b82b7017c3dd35b69ded490e
Denis Hennessy <denis@peerassembly.com>
2017-03-19 18:36:02 +0000

    Initial Commit

---
+++ HowToWebViewCell/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HowToWebViewCell
+//
+//  Created by Denis Hennessy on 19/03/2017.
+//  Copyright © 2017 Denis Hennessy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HowToWebViewCell/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HowToWebViewCell
+//
+//  Created by Denis Hennessy on 19/03/2017.
+//  Copyright © 2017 Denis Hennessy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
